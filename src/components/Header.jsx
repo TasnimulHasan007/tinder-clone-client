1e323ea3fd2a4ea0c3cb7e8aa17db5e652fa36af
Tasnimul Hasan <thtanim2005@gmail.com>
2021-11-06 18:37:34 +0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Person as PersonIcon, Forum as ForumIcon } from '@material-ui/icons'
+import { IconButton } from '@material-ui/core'
 import styles from './Header.module.css'
 
 const Header = () => {
-  return <header className={styles.header}></header>
+  return (
+    <header className={styles.header}>
+      <IconButton>
+        <PersonIcon fontSize="large" className={styles.header__icon} />
+      </IconButton>
+      <img
+        src="https://seeklogo.com/images/T/tinder-logo-FAAE852EC0-seeklogo.com.png"
+        alt=""
+        className={styles.header__logo}
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className={styles.header__icon} />
+      </IconButton>
+    </header>
+  )
 }
 
 export default Header
