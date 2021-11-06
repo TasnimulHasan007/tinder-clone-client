da522c53cda509fc7d131ca3f1209f1b5c3ef333
Tasnimul Hasan <thtanim2005@gmail.com>
2021-11-06 17:31:50 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="app">
+      {/* header */}
+      <Header />
+      {/* tinder cards */}
+      {/* swiper buttons */}
+    </div>
+  )
+}
+
+export default App
